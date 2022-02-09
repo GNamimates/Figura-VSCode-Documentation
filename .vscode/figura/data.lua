74bb74a9bc1231fd63699b55150908801c8496b7
GNamimates <greenneon188@gmail.com>
2022-02-10 02:23:36 +0800

    Data API complete! I think...

--- .vscode/figura/data.lua
+++ .vscode/figura/data.lua
@@ -1,10 +1,29 @@
 
 data = {}
 
----sets the target File name.
+---Sets the target File name.
 ---@param name string
 function data.setName(name) end
 
----gets the target File name.
+---Gets the target File name.
 ---@return string
 function data.getName(name) end
+
+---Saves a value in the file.
+---@param name any
+---@param value any
+function data.save(name, value) end
+
+---Returns a LuaValue of the key, or nil if not found.
+---@param name any
+function data.load(name) end
+
+---Returns a LuaTable of all the saved variables.
+function data.loadAll() end
+
+---Removes the value from the file.
+---@param name string
+function data.remove(name) end
+
+---Completely removes the file from the existence.
+function data.deleteFile() end
