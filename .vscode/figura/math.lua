67de4ca641d80f4d523fdabc9450e494dd7211af
GNamimates <greenneon188@gmail.com>
2022-02-10 02:26:25 +0800

    I forgor clamp in math

--- .vscode/figura/math.lua
+++ .vscode/figura/math.lua
@@ -8,3 +8,9 @@ math = {}
 ---@param x number
 ---@return number | Vector
 function math.lerp(a, b, x) end
+
+---Clamps the value.
+---@param value number
+---@param low number
+---@param high number
+function math.clamp(value,low,high) end
